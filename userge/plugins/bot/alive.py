c1ea94eaac2af7ee1eb0aef6ec328aa69a424a27
rafaxkk18 <92460839+rafaxkk18@users.noreply.github.com>
2021-10-13 15:31:53 -0300

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -227,11 +227,11 @@ class Bot_Alive:
     @staticmethod
     def alive_info() -> str:
         alive_info_ = f"""
-<a href="https://telegram.dog/x_xtests"><b>USERGE-X</a> is Up and Running.</b>
-  🐍   <b>Python :</b>    <code>v{versions.__python_version__}</code>
-  🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
-  🧬   <b>𝑿 :</b>    <code>v{get_version()}</code>
-<b>{Bot_Alive._get_mode()}</b>    <code>|</code>    🕔  <b>{userge.uptime}</b>
+-漫~*'¨¯¨'*·舞~ Cyber-X ~舞*'¨¯¨'*·~漫-
+  🐍   <b>Python ✧:･ﾟ</b> <code>v{versions.__python_version__}</code>
+  🔥   <b>Pyrogram ✧:･ﾟ</b> <code>v{versions.__pyro_version__}</code>
+  🧬   <b>𝑿 ✧:･ﾟ</b> <code>v{get_version()}</code>
+-漫~*'¨¯¨'*·舞~ Cyber-X ~舞*'¨¯¨'*·~漫-
 """
         return alive_info_
 
